0179479b61f8f1e07d3d270fc59b01d4a233ab76
grizzlih <ed.gizbreht@gmail.com>
2020-03-20 17:39:58 +0200

    Changed game config got the challenge

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-	public float fireRate = 0.5f;
-	public float nextFire = 0.0f;
+    public float spamDelay = 0.2f;
+    float nextSpawn = 0f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextSpawn)
         {
-			nextFire = Time.time + fireRate;
+            nextSpawn = Time.time + spamDelay;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
+
 }
